3bab4f42c776a79976f3a6a475db6e41c72bf0cb
liewvk <voonkiong@gamil.com>
2026-07-14 15:59:34 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace StudentRegistrationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudentId = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblDateOfBirth = new System.Windows.Forms.Label();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.cmbCourse = new System.Windows.Forms.ComboBox();
+            this.grpGender = new System.Windows.Forms.GroupBox();
+            this.rdoMale = new System.Windows.Forms.RadioButton();
+            this.rdoFemale = new System.Windows.Forms.RadioButton();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.chkAgree = new System.Windows.Forms.CheckBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.grpGender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblStudentId
+            // 
+            this.lblStudentId.AutoSize = true;
+            this.lblStudentId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentId.Location = new System.Drawing.Point(82, 82);
+            this.lblStudentId.Name = "lblStudentId";
+            this.lblStudentId.Size = new System.Drawing.Size(88, 20);
+            this.lblStudentId.TabIndex = 0;
+            this.lblStudentId.Text = "Student ID";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentName.Location = new System.Drawing.Point(234, 82);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(115, 20);
+            this.lblStudentName.TabIndex = 0;
+            this.lblStudentName.Text = "Student Name";
+            // 
+            // lblDateOfBirth
+            // 
+            this.lblDateOfBirth.AutoSize = true;
+            this.lblDateOfBirth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDateOfBirth.Location = new System.Drawing.Point(80, 185);
+            this.lblDateOfBirth.Name = "lblDateOfBirth";
+            this.lblDateOfBirth.Size = new System.Drawing.Size(105, 20);
+            this.lblDateOfBirth.TabIndex = 0;
+            this.lblDateOfBirth.Text = "Date of Birth";
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCourse.Location = new System.Drawing.Point(82, 234);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(68, 20);
+            this.lblCourse.TabIndex = 0;
+            this.lblCourse.Text = "Course:";
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStudentId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentId.Location = new System.Drawing.Point(77, 128);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(108, 24);
+            this.txtStudentId.TabIndex = 1;
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentName.Location = new System.Drawing.Point(239, 128);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(206, 24);
+            this.txtStudentName.TabIndex = 1;
+            // 
+            // dtpDateOfBirth
+            // 
+            this.dtpDateOfBirth.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDateOfBirth.Location = new System.Drawing.Point(204, 185);
+            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
+            this.dtpDateOfBirth.Size = new System.Drawing.Size(245, 22);
+            this.dtpDateOfBirth.TabIndex = 2;
+            // 
+            // cmbCourse
+            // 
+            this.cmbCourse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCourse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCourse.FormattingEnabled = true;
+            this.cmbCourse.Location = new System.Drawing.Point(187, 234);
+            this.cmbCourse.Name = "cmbCourse";
+            this.cmbCourse.Size = new System.Drawing.Size(200, 28);
+            this.cmbCourse.TabIndex = 3;
+            // 
+            // grpGender
+            // 
+            this.grpGender.Controls.Add(this.rdoFemale);
+            this.grpGender.Controls.Add(this.rdoMale);
+            this.grpGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpGender.Location = new System.Drawing.Point(483, 93);
+            this.grpGender.Name = "grpGender";
+            this.grpGender.Size = new System.Drawing.Size(221, 89);
+            this.grpGender.TabIndex = 4;
+            this.grpGender.TabStop = false;
+            this.grpGender.Text = "Gender";
+            // 
+            // rdoMale
+            // 
+            this.rdoMale.AutoSize = true;
+            this.rdoMale.Location = new System.Drawing.Point(18, 37);
+            this.rdoMale.Name = "rdoMale";
+            this.rdoMale.Size = new System.Drawing.Size(66, 24);
+            this.rdoMale.TabIndex = 0;
+            this.rdoMale.TabStop = true;
+            this.rdoMale.Text = "Male";
+            this.rdoMale.UseVisualStyleBackColor = true;
+            // 
+            // rdoFemale
+            // 
+            this.rdoFemale.AutoSize = true;
+            this.rdoFemale.Location = new System.Drawing.Point(100, 37);
+            this.rdoFemale.Name = "rdoFemale";
+            this.rdoFemale.Size = new System.Drawing.Size(85, 24);
+            this.rdoFemale.TabIndex = 1;
+            this.rdoFemale.TabStop = true;
+            this.rdoFemale.Text = "Female";
+            this.rdoFemale.UseVisualStyleBackColor = true;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(73, 312);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(51, 20);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.Location = new System.Drawing.Point(271, 312);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(56, 20);
+            this.lblPhone.TabIndex = 0;
+            this.lblPhone.Text = "Phone";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(73, 359);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(176, 24);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(275, 359);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(158, 24);
+            this.txtPhone.TabIndex = 1;
+            // 
+            // chkAgree
+            // 
+            this.chkAgree.AutoSize = true;
+            this.chkAgree.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAgree.Location = new System.Drawing.Point(67, 415);
+            this.chkAgree.Name = "chkAgree";
+            this.chkAgree.Size = new System.Drawing.Size(382, 24);
+            this.chkAgree.TabIndex = 5;
+            this.chkAgree.Text = "I confirm that the information entered is correct";
+            this.chkAgree.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(128, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(365, 40);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Student Registration System";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(67, 470);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(148, 35);
+            this.btnRegister.TabIndex = 7;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(224, 470);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(125, 35);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(365, 468);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 38);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbOutput.Location = new System.Drawing.Point(483, 211);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(221, 308);
+            this.rtbOutput.TabIndex = 10;
+            this.rtbOutput.Text = "Registration Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 553);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.chkAgree);
+            this.Controls.Add(this.grpGender);
+            this.Controls.Add(this.cmbCourse);
+            this.Controls.Add(this.dtpDateOfBirth);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.lblDateOfBirth);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblStudentId);
+            this.Name = "Form1";
+            this.Text = "Student Registration System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpGender.ResumeLayout(false);
+            this.grpGender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudentId;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblDateOfBirth;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.DateTimePicker dtpDateOfBirth;
+        private System.Windows.Forms.ComboBox cmbCourse;
+        private System.Windows.Forms.GroupBox grpGender;
+        private System.Windows.Forms.RadioButton rdoFemale;
+        private System.Windows.Forms.RadioButton rdoMale;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.CheckBox chkAgree;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentRegistrationSystem
+{
+    public partial class Form1 : Form
+    {
+        private bool IsValidPhoneNumber(string phone)
+        {
+            foreach (char c in phone)
+            {
+                if (!char.IsDigit(c) && c != ' ' && c != '+' && c != '-')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCourse.Items.Add("C# Programming");
+            cmbCourse.Items.Add("Web Development");
+            cmbCourse.Items.Add("Database Management");
+            cmbCourse.Items.Add("Mobile App Development");
+            cmbCourse.Items.Add("Artificial Intelligence");
+            cmbCourse.Items.Add("Cybersecurity");
+
+            cmbCourse.SelectedIndex = -1;
+            txtStudentId.Focus();
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string studentId = txtStudentId.Text.Trim();
+            string studentName = txtStudentName.Text.Trim();
+            string email = txtEmail.Text.Trim();
+            string phone = txtPhone.Text.Trim();
+
+            if (studentId == "")
+            {
+                MessageBox.Show("Please enter the Student ID.",
+                                "Missing Student ID",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtStudentId.Focus();
+                return;
+            }
+
+            if (studentName == "")
+            {
+                MessageBox.Show("Please enter the Student Name.",
+                                "Missing Student Name",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtStudentName.Focus();
+                return;
+            }
+
+            string gender = "";
+
+            if (rdoMale.Checked)
+            {
+                gender = "Male";
+            }
+            else if (rdoFemale.Checked)
+            {
+                gender = "Female";
+            }
+            else
+            {
+                MessageBox.Show("Please select a gender.",
+                                "Missing Gender",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            if (cmbCourse.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please select a course.",
+                                "Missing Course",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                cmbCourse.Focus();
+                return;
+            }
+
+            if (email == "")
+            {
+                MessageBox.Show("Please enter the email address.",
+                                "Missing Email",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtEmail.Focus();
+                return;
+            }
+
+            if (!email.Contains("@") || !email.Contains("."))
+            {
+                MessageBox.Show("Please enter a valid email address.",
+                                "Invalid Email",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtEmail.Focus();
+                return;
+            }
+
+            if (phone == "")
+            {
+                MessageBox.Show("Please enter the phone number.",
+                                "Missing Phone Number",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtPhone.Focus();
+                return;
+            }
+
+            if (!IsValidPhoneNumber(phone))
+            {
+                MessageBox.Show("Please enter a valid phone number.",
+                                "Invalid Phone Number",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtPhone.Focus();
+                return;
+            }
+
+            if (!chkAgree.Checked)
+            {
+                MessageBox.Show("Please confirm that the information entered is correct.",
+                                "Confirmation Required",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                chkAgree.Focus();
+                return;
+            }
+
+            string course = cmbCourse.SelectedItem.ToString();
+            DateTime dateOfBirth = dtpDateOfBirth.Value;
+
+            rtbOutput.Clear();
+
+            rtbOutput.AppendText("STUDENT REGISTRATION DETAILS\n");
+            rtbOutput.AppendText("----------------------------------------\n");
+            rtbOutput.AppendText($"Student ID     : {studentId}\n");
+            rtbOutput.AppendText($"Student Name   : {studentName}\n");
+            rtbOutput.AppendText($"Gender         : {gender}\n");
+            rtbOutput.AppendText($"Date of Birth  : {dateOfBirth.ToShortDateString()}\n");
+            rtbOutput.AppendText($"Course         : {course}\n");
+            rtbOutput.AppendText($"Email          : {email}\n");
+            rtbOutput.AppendText($"Phone Number   : {phone}\n");
+
+            MessageBox.Show("Student registration completed successfully.",
+                            "Registration Successful",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStudentId.Clear();
+            txtStudentName.Clear();
+            txtEmail.Clear();
+            txtPhone.Clear();
+
+            rdoMale.Checked = false;
+            rdoFemale.Checked = false;
+
+            cmbCourse.SelectedIndex = -1;
+            dtpDateOfBirth.Value = DateTime.Today;
+
+            chkAgree.Checked = false;
+
+            rtbOutput.Clear();
+
+            txtStudentId.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?",
+                                                  "Confirm Exit",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+
+
+
+
+
+     
+}
